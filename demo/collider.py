40acc237a1e367c757356e8754d11928b01b3f82
sugarflower <sugarflower@users.noreply.github.com>
2021-11-16 01:13:39 +0900

    Modify the calling library
    Changed from calling the old library to calling the current library.

--- demo/collider.py
+++ demo/collider.py
@@ -1,30 +1,59 @@
-from pyg import *
+from caya import *
 import random as rnd
-pd = PYGData()
 
-pd["a"] = {"x": 0, "y": 0, "w": 3, "h": 3}
-pd["b"] = {}
-for i in range(100):
-    pd["b"][i] = {"x": rnd.randint(-10,10),
-                  "y": rnd.randint(-10,10),
-                  "w": 3, "h": 3}
+class myApp(CAYA):
 
+    def collidrect(self, a, b):
+        if a["y"] >= b["y"]+b["h"]:
+            return False
+        elif a["x"]+a["w"] <= b["x"]:
+            return False
+        elif a["y"]+a["h"] <= b["y"]:
+            return False
+        elif a["x"] >= b["x"]+b["w"]:
+            return False
+        return True
 
-def collidrect(a, b):
-    if a["y"] >= b["y"]+b["h"]:
-        return False
-    elif a["x"]+a["w"] <= b["x"]:
-        return False
-    elif a["y"]+a["h"] <= b["y"]:
-        return False
-    elif a["x"] >= b["x"]+b["w"]:
-        return False
-    return True
+    def setup(self):
+        self.points = {}
+        self.points["a"] = {"x": 58, "y": 58, "w": 10, "h": 10}
+        self.spray()
+        self.config["waittime"] = 0.2
 
-hit = 0
-for i in range(100):
-    if collidrect(pd["a"], pd["b"][i]):
-        hit += 1
+    def spray(self):
+        self.points["b"] = {}
+        for i in range(100):
+            self.points["b"][i] = {"x": rnd.randint(0, 127),
+                                   "y": rnd.randint(0, 127),
+                                   "w": rnd.randint(3, 5),
+                                   "h": rnd.randint(3, 5)}
 
-print(hit)
-    
+    def loop(self):
+        self.clear()
+        hit = 0
+        for i in range(100):
+            if self.collidrect(self.points["a"],
+                               self.points["b"][i]):
+                hit += 1
+                color = (255, 0, 0)
+            else:
+                color = (100, 100, 100)
+                
+            box(self.getSurface(),
+                (self.points["b"][i]["x"],
+                 self.points["b"][i]["y"],
+                 self.points["b"][i]["w"],
+                 self.points["b"][i]["h"]),
+                 color)
+
+        self.spray()
+
+        rectangle(self.getSurface(),
+                  (self.points["a"]["x"],
+                   self.points["a"]["y"],
+                   self.points["a"]["w"],
+                   self.points["a"]["h"]),
+                  (255, 255, 255))
+            
+if __name__ == "__main__":
+    myApp()
